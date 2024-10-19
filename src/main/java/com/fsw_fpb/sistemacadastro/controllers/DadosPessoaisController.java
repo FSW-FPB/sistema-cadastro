ea7829a5b13dbfa9e0efb8de1d47cdf5fa5f066d
Reed0ne <danyelgianmarquesdasilva@gmail.com>
2024-10-19 10:32:02 -0300

    feat: findByID e findByCpf implementados

--- src/main/java/com/fsw_fpb/sistemacadastro/controllers/DadosPessoaisController.java
+++ src/main/java/com/fsw_fpb/sistemacadastro/controllers/DadosPessoaisController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +22,16 @@ public class DadosPessoaisController {
 
     @GetMapping
     public ResponseEntity<List<DadosPessoaisDTO>> findAll() {
-        List<DadosPessoais> dadosPessoaisList = service.findAll();
-        List<DadosPessoaisDTO> dadosPessoaisDTOList = dadosPessoaisList.stream()
-                .map(dados -> new DadosPessoaisDTO(dados))
-                .collect(Collectors.toList());
-        return new ResponseEntity<>(dadosPessoaisDTOList, HttpStatus.OK);
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/cpf/{cpf}")
+    public ResponseEntity<DadosPessoaisDTO> findByCpf(@PathVariable String cpf){
+        return ResponseEntity.ok(service.findByCpf(cpf));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DadosPessoaisDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
     }
 }
